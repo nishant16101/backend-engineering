e332174c32a3cf6e68d433a710d47921a5336ed7
nishant16101 <nishantkumbhar812@gmail.com>
2026-09-06 12:22:36 +0530

    models and schemas

--- project/app/models.py
+++ project/app/models.py
@@ -1,1 +1,24 @@
-s
+from sqlalchemy import String,Boolean,Text
+from sqlalchemy.orm import Mapped,mapped_column
+
+from database import Base
+
+class Task:
+    __tablename__ = "tasks"
+    id:Mapped[int] = mapped_column(
+        primary_key=True,
+        index=True
+    )
+    title:Mapped[str] = map(
+        String(200),
+        nullable=False
+    )
+    description: Mapped[str | None] = mapped_column(
+        Text,
+        nullable=True
+    )
+
+    completed: Mapped[bool] = mapped_column(
+        Boolean,
+        default=False
+    )
